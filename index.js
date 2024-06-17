d01d09a7f12647eb216ee42dcebe26a3ed8e4f72
ChervyachokMigo <github@svdgod.ru>
2024-06-18 01:49:32 +0500

    concat

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const calculation_progress = require('./calculation_progress');
+const osu_db_concat = require('./osu_db_concat');
 
-calculation_progress.launch({ osu_path: 'D:\\osu!' });
+const downoaded_db_path = 'C:\\Users\\sadgod\\Downloads\\osu! (1)';
+const osu_game_path = 'D:\\osu!';
+
+const args = process.argv.slice(2);
+
+if (args.length > 0) {
+	if (args.indexOf('concat') > -1){
+    	osu_db_concat.launch({ db1_path: osu_game_path, db2_path: downoaded_db_path });
+	}
+} else {
+	calculation_progress.launch({ osu_path: 'F:\\node_js_stuff\\node_projects\\osu_db_calculation' });
+}
+
+
---
+++ osu_db_concat.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const {osu_db_load, all_beatmap_properties, osu_db_save} = require('osu-tools');
+
+module.exports = {
+	launch: ({ db1_path, db2_path, osu_db_filename = 'osu!.db' }) => {
+
+		console.log('[ loading db 1 ]');
+		const result = osu_db_load( path.join(db1_path, osu_db_filename), all_beatmap_properties, { print_progress: true });
+
+		console.log('[ loading db 2 ]');
+		const osu_db_2_result = osu_db_load( path.join(db2_path, osu_db_filename), all_beatmap_properties, { print_progress: true });
+
+		const srs = ['star_rating_std', 'star_rating_taiko', 'star_rating_ctb', 'star_rating_mania'];
+
+        console.log('[ comparing ]');
+
+		for (let i = 0; i < result.beatmaps.length; i++){
+			if (i % 1000 == 0) {
+				console.log('compare', i, '/', result.beatmaps.length, `${(i/result.beatmaps.length*100).toFixed(2)}` ,'maps');
+			}
+
+			if ( result.beatmaps[i].beatmap_md5.length !== 32) {
+				continue;
+			}
+
+			const idx = osu_db_2_result.beatmaps.findIndex( v => v.beatmap_md5 === result.beatmaps[i].beatmap_md5 );
+
+			if (idx == -1) {
+				continue;
+			}
+
+			//console.log('found beatmap', osu_db_2_result.beatmaps[idx].beatmap_md5 );
+
+			for (let sr of srs) {
+				if (result.beatmaps[i][sr].length == 0) {
+					if (osu_db_2_result.beatmaps[idx][sr].length > 0) {
+						result.beatmaps[i][sr] = osu_db_2_result.beatmaps[idx][sr];
+					}
+                }
+			}
+		}
+
+		console.log('[ saving ]');
+		osu_db_save(result, 'osu!.db');
+
+	}
+}
